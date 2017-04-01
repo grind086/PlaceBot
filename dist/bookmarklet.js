580c8447ce7fd7e5b96b48b20e7975ee576baec1
Rob <rgrindeland@gmail.com>
2017-04-01 02:08:36 +0000

    Rebuilt

--- dist/bookmarklet.js
+++ dist/bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function() {
 var s = document.createElement('script');
 s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'https://raw.githubusercontent.com/grind086/PlaceBot/master/dist/placebot.min.js');
+s.setAttribute('src', 'https://grind086.github.io/PlaceBot/dist/placebot.min.js');
 document.head.appendChild(s);
 })();
--- dist/placebot.user.js
+++ dist/placebot.user.js
@@ -3,13 +3,13 @@
 // @version     0.0.2
 // @namespace   https://github.com/grind086/PlaceBot
 // @description A bot that automates drawing on reddit.com/r/place
-// @include     http://www.reddit.com/r/place
-// @include     https://www.reddit.com/r/place
+// @include     http://www.reddit.com/r/place/
+// @include     https://www.reddit.com/r/place/
 // ==/UserScript==
 
 (function() {
 var s = document.createElement('script');
 s.setAttribute('type', 'text/javascript');
-s.setAttribute('src', 'https://raw.githubusercontent.com/grind086/PlaceBot/master/dist/placebot.min.js');
+s.setAttribute('src', 'https://grind086.github.io/PlaceBot/dist/placebot.min.js');
 document.head.appendChild(s);
 })();
