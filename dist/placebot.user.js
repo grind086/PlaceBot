19af519d1ac63322906cd56f7a8d1291ba9d21ab
Rob <rgrindeland@gmail.com>
2017-04-01 02:11:21 +0000

    Bump version number to fix greasemonkey

--- dist/placebot.user.js
+++ dist/placebot.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        PlaceBot
-// @version     0.0.2
+// @version     0.0.3
 // @namespace   https://github.com/grind086/PlaceBot
 // @description A bot that automates drawing on reddit.com/r/place
 // @include     http://www.reddit.com/r/place/
